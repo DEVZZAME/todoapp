f531a16ac7642b2fe7d32725aa3f0252ee1321f7
zzame <dev.zzame@gmail.com>
2022-05-03 05:19:02 +0900

    Login Service

--- server.js
+++ server.js
@@ -94,3 +94,63 @@ app.put('/edit', function(req, res){
     res.redirect('/list');
   })
 });
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({ secret : '비밀코드', resave : true, saveUninitialized : false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(req, res){
+  res.render('login.ejs')
+});
+
+app.post('/login', passport.authenticate('local', {
+  failureRedirect : '/fail'
+} ), function(req, res){
+  res.redirect('/');
+});
+
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지 않는 아이디 입니다.' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비밀번호를 확인해 주십시오.' })
+    }
+  })
+}));
+
+passport.serializeUser(function(user, done){
+  done(null, user.id)
+});
+
+passport.deserializeUser(function(아이디, done){
+  db.collection('login').findOne({id : 아이디}, function(에러, 결과){
+    done(null, 결과)
+  })
+  
+});
+
+app.get('/mypage', loginCheck, function(req, res){
+  res.render('mypage.ejs', {사용자 : req.user})
+});
+
+function loginCheck(req, res, next){
+  if(req.user){
+    next();
+  }else{
+    res.send('먼저 로그인을 해주십시오.');
+  }
+}
