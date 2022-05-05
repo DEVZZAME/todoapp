2dc1ea240397ad3ef1d15e78e6dbd8bc09cfe27b
zzame <dev.zzame@gmail.com>
2022-05-06 01:25:26 +0900

    Upload Service

--- server.js
+++ server.js
@@ -195,6 +195,40 @@ app.get('/search', function (req, res) {
   })
 });
 
+let multer = require('multer');
+var storage = multer.diskStorage({
+  destination : function(req, file, cb){
+    cb(null, './public/image')
+  },
+  filename : function(req, file, cb){
+    cb(null, file.originalname)
+  },
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+        return callback(new Error('PNG, JPG만 업로드하세요'))
+    }
+    callback(null, true)
+    },
+    limits:{
+        fileSize: 1920 * 1080
+    }
+});
+
+var upload = multer({storage : storage});
+
+app.get('/upload', function(req, res){
+  res.render('upload.ejs')
+});
+
+app.post('/upload', upload.single('uploadTest'), function(req, res){
+  res.send('업로드 완료');
+})
+
+app.get('/image/:imageName', function(요청, 응답){
+  응답.sendFile( __dirname + '/public/image/' + 요청.params.imageName )
+})
+
 function loginCheck(req, res, next) {
   if (req.user) {
     next();
